c0fb24a584e73e4d51c0227f49448700fc9b6551
Samuel Lloyd-Lindholm <samuel.lloyd-lindholm@abex.capital>
2024-03-22 11:57:29 +0000

    lazyvim based config

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,12 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
